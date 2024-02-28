f530c791e9651a61c946b19d6c56aaf2c8c8b197
trung.nguyendinh <nguyendinhtrung.it@gmail.com>
2024-02-28 19:55:14 +0700

    Up load all file start

---
+++ src/contexts/pages/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const PageContexts = () => {
+  return <div>PageContexts</div>;
+};
---
+++ src/services/pages/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const PageServices = () => {
+  return <div>PageServices</div>;
+};
