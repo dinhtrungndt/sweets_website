da6e1d183063350b910c8c2f25855208650bd8fe
Nguyễn Đình Trưng <127390593+dinhtrungndt@users.noreply.github.com>
2024-04-05 23:22:03 +0700

    up load pull vi

--- src/components/pages/login/index.js
+++ src/components/pages/login/index.js
@@ -2,7 +2,10 @@ import React, { useState, useEffect, useRef } from "react";
 import QRCode from "qrcode.react";
 import { encode } from "base-64";
 import io from "socket.io-client";
-import { CreateDevice, UpdateDevice } from "../../../services/QRCode/QRCodeService";
+import {
+  CreateDevice,
+  UpdateDevice,
+} from "../../../services/QRCode/QRCodeService";
 import { useNavigate } from "react-router-dom";
 import { Alert } from "bootstrap";
 import { Button, Input } from "antd";
@@ -24,27 +27,26 @@ function QRCodeGenerator() {
   const encodedDateTime = encode(dateTime.toISOString());
 
   useEffect(() => {
-    socket.current = io("http://192.168.1.45:3001/");
+    socket.current = io("https://sweets-nodejs.onrender.com/");
     socket.current.on("connect", () => {
       console.log("Connected to server");
     });
-    socket.current.on('send_device_iduser2', (data) => {
-      console.log('>>>>>>>>>> data.iduser : ', data.iduser);
-      console.log('>>>>>>>>>> data.deviceid : ', data.deviceid);
-      console.log('>>>>>>>>>> encodedDateTime : ', encodedDateTime);
+    socket.current.on("send_device_iduser2", (data) => {
+      console.log(">>>>>>>>>> data.iduser : ", data.iduser);
+      console.log(">>>>>>>>>> data.deviceid : ", data.deviceid);
+      console.log(">>>>>>>>>> encodedDateTime : ", encodedDateTime);
       if (data.deviceid !== encodedDateTime) {
         return;
-
       }
       const CheckDeviceUpdate = async () => {
         const response = await UpdateDevice(data.iduser, data.deviceid);
         if (response.status) {
-          console.log('đăng nhập thành công');
-          localStorage.setItem('iduser', data.iduser); // Lưu iduser vào LocalStorage
-          navigate('/posts');
-          alert('Đăng nhập thành công');
+          console.log("đăng nhập thành công");
+          localStorage.setItem("iduser", data.iduser); // Lưu iduser vào LocalStorage
+          navigate("/posts");
+          alert("Đăng nhập thành công");
         }
-      }
+      };
       CheckDeviceUpdate();
     });
     if (!onCreateDeviceCalled.current) {
@@ -65,70 +67,50 @@ function QRCodeGenerator() {
   const CheckDeviceUpdate = async (iduser, deviceid) => {
     const response = await UpdateDevice(iduser, deviceid);
     if (response.status) {
-      console.log('đăng nhập thành công');
-      localStorage.setItem('iduser', iduser); // Lưu iduser vào LocalStorage
+      console.log("đăng nhập thành công");
+      localStorage.setItem("iduser", iduser); // Lưu iduser vào LocalStorage
     }
     return response;
-  }
+  };
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
-
 
   return (
     <div className="containerlogin">
       <div className="itemlogin">
         <div className="leftlogin">
           <div className="logoitem">
-            <div className="txtwelcome">
-              Welcome back
-            </div>
+            <div className="txtwelcome">Welcome back</div>
             <img src={logone} className="logo" />
           </div>
           <div className="txtcontent">
             Chào mừng bạn quay trở lại với chúng tôi
           </div>
           <div className="email1">
-            <div>
-              Nhập tài khoản Email
-            </div>
-            <Input
-              className="inputemail"
-            />
+            <div>Nhập tài khoản Email</div>
+            <Input className="inputemail" />
           </div>
           <div className="email">
-            <div>
-              Nhập mật khẩu
-            </div>
+            <div>Nhập mật khẩu</div>
             <Input
-              type={showPassword ? 'text' : 'password'}
+              type={showPassword ? "text" : "password"}
               className="inputemail"
             />
-            {
-              showPassword ? (
-                <EyeOutlined
-                  className="eye"
-                  onClick={togglePasswordVisibility}
-                />
-              ) : (
-                <EyeInvisibleOutlined
-                  className="eye"
-                  onClick={togglePasswordVisibility}
-                />
-              )
-            }
+            {showPassword ? (
+              <EyeOutlined className="eye" onClick={togglePasswordVisibility} />
+            ) : (
+              <EyeInvisibleOutlined
+                className="eye"
+                onClick={togglePasswordVisibility}
+              />
+            )}
           </div>
-          <div className="forgotpass">
-            Quên mật khẩu?
-          </div>
+          <div className="forgotpass">Quên mật khẩu?</div>
           <Button className="btnlogin">Đăng nhập</Button>
           <Link to={`/register`}>
-            <div className="forgotpass">
-              Đăng ký tài khoản
-            </div>
+            <div className="forgotpass">Đăng ký tài khoản</div>
           </Link>
-
-
         </div>
         <div className="rightlogin">
           {dateTime && (
@@ -136,19 +118,11 @@ function QRCodeGenerator() {
               <QRCode value={encodedDateTime} />
             </div>
           )}
-          <div className="txtloginqr">
-            Login bằng mã QR
-          </div>
-          <div className="txtqr">
-            Sử dụng ứng dụng Sweet
-          </div>
-          <div className="txtqr">
-            để quét mã QR để đăng nhập
-          </div>
+          <div className="txtloginqr">Login bằng mã QR</div>
+          <div className="txtqr">Sử dụng ứng dụng Sweet</div>
+          <div className="txtqr">để quét mã QR để đăng nhập</div>
         </div>
       </div>
-
-
     </div>
   );
 }
